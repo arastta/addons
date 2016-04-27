02360213e0dfd45740a013f7c14d80f293539bf3
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 16:25:30 +0200

    Paypoint adhere coding standard

--- Payments/paypoint/upload/catalog/model/payment/paypoint.php
+++ Payments/paypoint/upload/catalog/model/payment/paypoint.php
@@ -1,31 +1,39 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
-class ModelPaymentPayPoint extends Model {
-	public function getMethod($address, $total) {
-		$this->load->language('payment/paypoint');
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('paypoint_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+class ModelPaymentPayPoint extends Model
+{
+    public function getMethod($address, $total)
+    {
+        $this->load->language('payment/paypoint');
 
-		if ($this->config->get('paypoint_total') > 0 && $this->config->get('paypoint_total') > $total) {
-			$status = false;
-		} elseif (!$this->config->get('paypoint_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get('paypoint_geo_zone_id') . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
 
-		$method_data = array();
+        if ($this->config->get('paypoint_total') > 0 && $this->config->get('paypoint_total') > $total) {
+            $status = false;
+        } elseif (!$this->config->get('paypoint_geo_zone_id')) {
+            $status = true;
+        } elseif ($query->num_rows) {
+            $status = true;
+        } else {
+            $status = false;
+        }
 
-		if ($status) {
-			$method_data = array(
-				'code'       => 'paypoint',
-				'title'      => $this->language->get('text_title'),
-				'terms'      => '',
-				'sort_order' => $this->config->get('paypoint_sort_order')
-			);
-		}
+        $method_data = array();
 
-		return $method_data;
-	}
+        if ($status) {
+            $method_data = array(
+                'code'       => 'paypoint',
+                'title'      => $this->language->get('text_title'),
+                'terms'      => '',
+                'sort_order' => $this->config->get('paypoint_sort_order')
+            );
+        }
+
+        return $method_data;
+    }
 }
