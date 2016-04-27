aea2c18285064584b62b18654d0d8042e58fe96b
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 10:08:47 +0200

    Amazon Button adhere coding standard

--- Modules/amazon_button/upload/admin/language/en-GB/module/amazon_button.php
+++ Modules/amazon_button/upload/admin/language/en-GB/module/amazon_button.php
@@ -1,18 +1,24 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
-// Heading
-$_['heading_title']    = 'Amazon Payments Button';
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
 
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified Amazon payments module!';
-$_['text_edit']        = 'Edit Amazon Payments Module';
-$_['text_left']        = 'Left';
-$_['text_right']       = 'Right';
-$_['text_center']      = 'Center';
+// Heading
+$_['heading_title'] = 'Amazon Payments Button';
+
+$_['text_module']  = 'Modules';
+$_['text_success'] = 'Success: You have modified Amazon payments module!';
+$_['text_edit']    = 'Edit Amazon Payments Module';
+$_['text_left']    = 'Left';
+$_['text_right']   = 'Right';
+$_['text_center']  = 'Center';
 
 // Entry
-$_['entry_name']       = 'Module Name';
-$_['entry_align']      = 'Align';
-$_['entry_status']     = 'Status';
+$_['entry_name']   = 'Module Name';
+$_['entry_align']  = 'Align';
+$_['entry_status'] = 'Status';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify Amazon payments module!';
