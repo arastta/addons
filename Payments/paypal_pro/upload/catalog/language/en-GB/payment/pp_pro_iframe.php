e6db3f9ed319704650ed00af54c9e64c22b7970a
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 16:20:46 +0200

    Paypal pro adhere coding standard

--- Payments/paypal_pro/upload/catalog/language/en-GB/payment/pp_pro_iframe.php
+++ Payments/paypal_pro/upload/catalog/language/en-GB/payment/pp_pro_iframe.php
@@ -1,7 +1,13 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
+
 // Text
-$_['text_title']				= 'Credit or Debit Card';
-$_['text_secure_connection']	= 'Creating a secure connection...';
+$_['text_title']             = 'Credit or Debit Card';
+$_['text_secure_connection'] = 'Creating a secure connection...';
 
 // Error
-$_['error_connection']			= 'Could not connect to PayPal. Please contact the shop\'s administrator for assistance or choose a different payment method.';
+$_['error_connection'] = 'Could not connect to PayPal. Please contact the shop\'s administrator for assistance or choose a different payment method.';
--- Payments/paypal_pro/upload/catalog/model/payment/pp_pro.php
+++ Payments/paypal_pro/upload/catalog/model/payment/pp_pro.php
@@ -1,31 +1,39 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
-class ModelPaymentPPPro extends Model {
-	public function getMethod($address, $total) {
-		$this->language->load('payment/pp_pro');
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('pp_pro_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+class ModelPaymentPPPro extends Model
+{
+    public function getMethod($address, $total)
+    {
+        $this->language->load('payment/pp_pro');
 
-		if ($this->config->get('pp_pro_total') > 0 && $this->config->get('pp_pro_total') > $total) {
-			$status = false;
-		} elseif (!$this->config->get('pp_pro_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get('pp_pro_geo_zone_id') . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
 
-		$method_data = array();
+        if ($this->config->get('pp_pro_total') > 0 && $this->config->get('pp_pro_total') > $total) {
+            $status = false;
+        } elseif (!$this->config->get('pp_pro_geo_zone_id')) {
+            $status = true;
+        } elseif ($query->num_rows) {
+            $status = true;
+        } else {
+            $status = false;
+        }
 
-		if ($status) {
-			$method_data = array(
-				'code'       => 'pp_pro',
-				'title'      => $this->language->get('text_title'),
-				'terms'      => '',
-				'sort_order' => $this->config->get('pp_pro_sort_order')
-			);
-		}
+        $method_data = array();
 
-		return $method_data;
-	}
+        if ($status) {
+            $method_data = array(
+                'code'       => 'pp_pro',
+                'title'      => $this->language->get('text_title'),
+                'terms'      => '',
+                'sort_order' => $this->config->get('pp_pro_sort_order')
+            );
+        }
+
+        return $method_data;
+    }
 }
