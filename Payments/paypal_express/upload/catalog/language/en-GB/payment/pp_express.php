8e71072058b8c823caa751c13d4d103dc041ef52
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 14:54:25 +0200

    Paypal Express adhere coding standard

--- Payments/paypal_express/upload/catalog/language/en-GB/payment/pp_express.php
+++ Payments/paypal_express/upload/catalog/language/en-GB/payment/pp_express.php
@@ -1,19 +1,25 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
+
 // Heading
-$_['express_text_title']      = 'Confirm order';
+$_['express_text_title'] = 'Confirm order';
 
 // Text
-$_['text_title']              = 'PayPal Express Checkout';
-$_['button_continue']         = 'Continue';
-$_['text_cart']               = 'Shopping Cart';
-$_['text_shipping_updated']   = 'Shipping service updated';
-$_['text_trial']              = '%s every %s %s for %s payments then ';
-$_['text_recurring']          = '%s every %s %s';
-$_['text_recurring_item']     = 'Recurring Item';
-$_['text_length']             = ' for %s payments';
+$_['text_title']            = 'PayPal Express Checkout';
+$_['button_continue']       = 'Continue';
+$_['text_cart']             = 'Shopping Cart';
+$_['text_shipping_updated'] = 'Shipping service updated';
+$_['text_trial']            = '%s every %s %s for %s payments then ';
+$_['text_recurring']        = '%s every %s %s';
+$_['text_recurring_item']   = 'Recurring Item';
+$_['text_length']           = ' for %s payments';
 
 // Entry
-$_['express_entry_coupon']    = 'Enter your coupon here:';
+$_['express_entry_coupon'] = 'Enter your coupon here:';
 
 // Button
 $_['button_express_coupon']   = 'Add';
@@ -23,5 +29,5 @@ $_['button_express_shipping'] = 'Update shipping';
 $_['button_cancel_recurring'] = 'Cancel payments';
 
 // Error
-$_['error_heading_title']	  = 'There was an error';
+$_['error_heading_title']     = 'There was an error';
 $_['error_too_many_failures'] = 'Your payment has failed too many times';
