d11709345e5081679a4abda5bfc1b313f1b05156
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 18:18:31 +0200

    Update Copyright Date All Files

--- Payments/realex/upload/admin/controller/payment/realex_remote.php
+++ Payments/realex/upload/admin/controller/payment/realex_remote.php
@@ -2,7 +2,7 @@
 
 /**
  * @package        Arastta eCommerce
- * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @copyright      Copyright (C) 2015-2016 Arastta Association. All rights reserved. (arastta.org)
  * @license        GNU General Public License version 3; see LICENSE.txt
  */
 class ControllerPaymentRealexRemote extends Controller
