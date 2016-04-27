31055a38ff36460538d0b2be8b8a2dfda38e2f06
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 10:53:24 +0200

    Paypal login adhere coding standard

--- Modules/paypal_login/upload/catalog/model/module/pp_login.php
+++ Modules/paypal_login/upload/catalog/model/module/pp_login.php
@@ -1,82 +1,94 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
-class ModelModulePPLogin extends Model {
-	public function getTokens($code) {
-		if ($this->config->get('pp_login_sandbox')) {
-			$endpoint = 'https://api.sandbox.paypal.com/v1/oauth2/token';
-		} else {
-			$endpoint = 'https://api.paypal.com/v1/oauth2/token';
-		}
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
 
-		$request  = '';
-		$request .= 'client_id=' . $this->config->get('pp_login_client_id');
-		$request .= '&client_secret=' . $this->config->get('pp_login_secret');
-		$request .= '&grant_type=authorization_code';
-		$request .= '&code=' . $code;
-		$request .= '&redirect_uri=' . urlencode($this->url->link('module/pp_login/login', '', 'SSL'));
+class ModelModulePPLogin extends Model
+{
+    public function getTokens($code)
+    {
+        if ($this->config->get('pp_login_sandbox')) {
+            $endpoint = 'https://api.sandbox.paypal.com/v1/oauth2/token';
+        } else {
+            $endpoint = 'https://api.paypal.com/v1/oauth2/token';
+        }
 
-		$additional_opts = array(
-			CURLOPT_USERPWD    => $this->config->get('pp_login_client_id') . ':' . $this->config->get('pp_login_secret'),
-			CURLOPT_POST       => true,
-			CURLOPT_POSTFIELDS => $request
-		);
+        $request = '';
+        $request .= 'client_id=' . $this->config->get('pp_login_client_id');
+        $request .= '&client_secret=' . $this->config->get('pp_login_secret');
+        $request .= '&grant_type=authorization_code';
+        $request .= '&code=' . $code;
+        $request .= '&redirect_uri=' . urlencode($this->url->link('module/pp_login/login', '', 'SSL'));
 
-		$curl = $this->curl($endpoint, $additional_opts);
+        $additional_opts = array(
+            CURLOPT_USERPWD    => $this->config->get('pp_login_client_id') . ':' . $this->config->get('pp_login_secret'),
+            CURLOPT_POST       => true,
+            CURLOPT_POSTFIELDS => $request
+        );
 
-		$this->log('cURL Response: ' . print_r($curl, 1));
+        $curl = $this->curl($endpoint, $additional_opts);
 
-		return $curl;
-	}
+        $this->log('cURL Response: ' . print_r($curl, 1));
 
-	public function getUserInfo($access_token) {
-		if ($this->config->get('pp_login_sandbox')) {
-			$endpoint = 'https://api.sandbox.paypal.com/v1/identity/openidconnect/userinfo/?schema=openid';
-		} else {
-			$endpoint = 'https://api.paypal.com/v1/identity/openidconnect/userinfo/?schema=openid';
-		}
+        return $curl;
+    }
 
-		$header   = array();
-		$header[] = 'Content-Type: application/json';
-		$header[] = 'Authorization: Bearer ' . $access_token;
+    public function getUserInfo($access_token)
+    {
+        if ($this->config->get('pp_login_sandbox')) {
+            $endpoint = 'https://api.sandbox.paypal.com/v1/identity/openidconnect/userinfo/?schema=openid';
+        } else {
+            $endpoint = 'https://api.paypal.com/v1/identity/openidconnect/userinfo/?schema=openid';
+        }
 
-		$additional_opts = array(
-			CURLOPT_HTTPHEADER => $header,
-		);
+        $header   = array();
+        $header[] = 'Content-Type: application/json';
+        $header[] = 'Authorization: Bearer ' . $access_token;
 
-		$curl = $this->curl($endpoint, $additional_opts);
+        $additional_opts = array(
+            CURLOPT_HTTPHEADER => $header,
+        );
 
-		$this->log('cURL Response: ' . print_r($curl, 1));
+        $curl = $this->curl($endpoint, $additional_opts);
 
-		return $curl;
-	}
+        $this->log('cURL Response: ' . print_r($curl, 1));
 
-	private function curl($endpoint, $additional_opts = array()) {
-		$default_opts = array(
-			CURLOPT_PORT           => 443,
-			CURLOPT_HEADER         => 0,
-			CURLOPT_SSL_VERIFYPEER => 0,
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_FORBID_REUSE   => 1,
-			CURLOPT_FRESH_CONNECT  => 1,
-			CURLOPT_URL            => $endpoint,
-		);
+        return $curl;
+    }
 
-		$ch = curl_init($endpoint);
+    private function curl($endpoint, $additional_opts = array())
+    {
+        $default_opts = array(
+            CURLOPT_PORT           => 443,
+            CURLOPT_HEADER         => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FORBID_REUSE   => 1,
+            CURLOPT_FRESH_CONNECT  => 1,
+            CURLOPT_URL            => $endpoint,
+        );
 
-		$opts = $default_opts + $additional_opts;
+        $ch = curl_init($endpoint);
 
-		curl_setopt_array($ch, $opts);
+        $opts = $default_opts + $additional_opts;
 
-		$response = json_decode(curl_exec($ch));
+        curl_setopt_array($ch, $opts);
 
-		curl_close($ch);
+        $response = json_decode(curl_exec($ch));
 
-		return $response;
-	}
+        curl_close($ch);
 
-	public function log($data) {
-		if ($this->config->get('pp_login_debug')) {
-			$backtrace = debug_backtrace();
-			$this->log->write('Log In with PayPal debug (' . $backtrace[1]['class'] . '::' . $backtrace[1]['function'] . ') - ' . $data);
-		}
-	}
+        return $response;
+    }
+
+    public function log($data)
+    {
+        if ($this->config->get('pp_login_debug')) {
+            $backtrace = debug_backtrace();
+
+            $this->log->write('Log In with PayPal debug (' . $backtrace[1]['class'] . '::' . $backtrace[1]['function'] . ') - ' . $data);
+        }
+    }
 }
