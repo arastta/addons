54a587d91dec4c77a3040f2ed5e518ccf52c8b85
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 11:43:42 +0200

    Citylink adhere coding standard

--- Shippings/citylink/upload/catalog/model/shipping/citylink.php
+++ Shippings/citylink/upload/catalog/model/shipping/citylink.php
@@ -1,59 +1,67 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
-class ModelShippingCitylink extends Model {
-	function getQuote($address) {
-		$this->load->language('shipping/citylink');
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('citylink_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+class ModelShippingCitylink extends Model
+{
+    public function getQuote($address)
+    {
+        $this->load->language('shipping/citylink');
 
-		if (!$this->config->get('citylink_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get('citylink_geo_zone_id') . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
 
-		$method_data = array();
+        if (!$this->config->get('citylink_geo_zone_id')) {
+            $status = true;
+        } elseif ($query->num_rows) {
+            $status = true;
+        } else {
+            $status = false;
+        }
 
-		if ($status) {
-			$cost = 0;
-			$weight = $this->cart->getWeight();
+        $method_data = array();
 
-			$rates = explode(',', $this->config->get('citylink_rate'));
+        if ($status) {
+            $cost   = 0;
+            $weight = $this->cart->getWeight();
 
-			foreach ($rates as $rate) {
-				$data = explode(':', $rate);
+            $rates = explode(',', $this->config->get('citylink_rate'));
 
-				if ($data[0] >= $weight) {
-					if (isset($data[1])) {
-						$cost = $data[1];
-					}
+            foreach ($rates as $rate) {
+                $data = explode(':', $rate);
 
-					break;
-				}
-			}
+                if ($data[0] >= $weight) {
+                    if (isset($data[1])) {
+                        $cost = $data[1];
+                    }
 
-			$quote_data = array();
+                    break;
+                }
+            }
 
-			if ((float)$cost) {
-				$quote_data['citylink'] = array(
-					'code'         => 'citylink.citylink',
-					'title'        => $this->language->get('text_title') . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class_id')) . ')',
-					'cost'         => $cost,
-					'tax_class_id' => $this->config->get('citylink_tax_class_id'),
-					'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('citylink_tax_class_id'), $this->config->get('config_tax')))
-				);
+            $quote_data = array();
 
-				$method_data = array(
-					'code'       => 'citylink',
-					'title'      => $this->language->get('text_title'),
-					'quote'      => $quote_data,
-					'sort_order' => $this->config->get('citylink_sort_order'),
-					'error'      => false
-				);
-			}
-		}
+            if ((float) $cost) {
+                $quote_data['citylink'] = array(
+                    'code'         => 'citylink.citylink',
+                    'title'        => $this->language->get('text_title') . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class_id')) . ')',
+                    'cost'         => $cost,
+                    'tax_class_id' => $this->config->get('citylink_tax_class_id'),
+                    'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('citylink_tax_class_id'), $this->config->get('config_tax')))
+                );
 
-		return $method_data;
-	}
+                $method_data = array(
+                    'code'       => 'citylink',
+                    'title'      => $this->language->get('text_title'),
+                    'quote'      => $quote_data,
+                    'sort_order' => $this->config->get('citylink_sort_order'),
+                    'error'      => false
+                );
+            }
+        }
+
+        return $method_data;
+    }
 }
