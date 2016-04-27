e92db2d90559a068df8db4f4a9e03354214bb6e9
cuneytsenturk <cneytsenturk@gmail.com>
2016-04-27 10:41:24 +0200

    Paypal Button adhere coding standard

--- Modules/paypal_button/upload/admin/language/en-GB/module/pp_button.php
+++ Modules/paypal_button/upload/admin/language/en-GB/module/pp_button.php
@@ -1,14 +1,20 @@
-<?php/** * @package		Arastta eCommerce * @copyright	Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org) * @license		GNU General Public License version 3; see LICENSE.txt */
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015 Arastta Association. All rights reserved. (arastta.org)
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
+
 // Heading
-$_['heading_title']    = 'PayPal Express Checkout Button';
+$_['heading_title'] = 'PayPal Express Checkout Button';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified PayPal Express Checkout Button module!';
-$_['text_edit']        = 'Edit PayPal Express Checkout Button Module';
+$_['text_module']  = 'Modules';
+$_['text_success'] = 'Success: You have modified PayPal Express Checkout Button module!';
+$_['text_edit']    = 'Edit PayPal Express Checkout Button Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status'] = 'Status';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify PayPal Express Checkout Button module!';
