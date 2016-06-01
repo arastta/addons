5c602adc038bf2c0061a3ca8fe1a53f0898fc7f5
cuneytsenturk <cneytsenturk@gmail.com>
2016-06-01 15:52:19 +0300

    Added New Module: Laybuy Layout

---
+++ Modules/laybuy_layout/upload/admin/controller/module/laybuy_layout.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package        Arastta eCommerce
+ * @copyright      Copyright (C) 2015-2016 Arastta Association. All rights reserved. (arastta.org)
+ * @credits        See CREDITS.txt for credits and other copyright notices.
+ * @license        GNU General Public License version 3; see LICENSE.txt
+ */
+
+class ControllerModuleLaybuyLayout extends Controller
+{
+    private $error = array();
+
+    public function index()
+    {
+        $this->load->model('setting/setting');
+
+        $this->language->load('module/laybuy_layout');
+
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            $this->model_setting_setting->editSetting('laybuy_layout', $this->request->post);
+
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            if (isset($this->request->post['button']) and $this->request->post['button'] == 'save') {
+                $this->response->redirect($this->url->link($this->request->get['route'], 'token=' . $this->session->data['token'], 'SSL'));
+            }
+
+            $this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], true));
+        }
+
+        $data = $this->language->all();
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        $data['action'] = $this->url->link('module/laybuy_layout', 'token=' . $this->session->data['token'], true);
+
+        $data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], true);
+
+        if (isset($this->request->post['laybuy_layout_status'])) {
+            $data['laybuy_layout_status'] = $this->request->post['laybuy_layout_status'];
+        } else {
+            $data['laybuy_layout_status'] = $this->config->get('laybuy_layout_status');
+        }
+
+        $data['header']      = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer']      = $this->load->controller('common/footer');
+
+        $this->response->setOutput($this->load->view('module/laybuy_layout.tpl', $data));
+    }
+
+    protected function validate()
+    {
+        if (!$this->user->hasPermission('modify', 'module/laybuy_layout')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        return !$this->error;
+    }
+}
