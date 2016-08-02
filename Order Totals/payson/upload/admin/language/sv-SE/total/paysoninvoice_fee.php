d39f6977c958d65570e3eccea5f3d954621862d3
cuneytsenturk <cneytsenturk@gmail.com>
2016-08-02 10:21:00 +0300

    Added New Order Total: Payson

---
+++ Order Totals/payson/upload/admin/language/sv-SE/total/paysoninvoice_fee.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title'] = 'Payson Fakturaavgift';
+
+// Text
+$_['text_total']   = 'Order Totals';
+$_['text_success'] = 'Klart: Inställningarna har ändrats!';
+$_['text_edit']    = 'Uppdatera Payson faktura avgift';
+
+// Entry
+$_['entry_total']      = 'Order Total';
+$_['entry_fee']        = 'Avgift';
+$_['entry_tax_class']  = 'Momsklass';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sorteringsordning';
+
+// Error
+$_['error_permission'] = 'Varning: Du har inte behörighet att ändra i avgifter hantering!';
+
+//Help
+$_['help_fee']       = 'Retail-priset med moms måste vara i spannet 0 - 40SEK <br />Momsen m?ste vara 25%';
+$_['help_tax_class'] = 'Momsen m?ste vara 25%';
---
+++ Order Totals/payson/upload/catalog/language/sv-SE/total/paysoninvoice_fee.php
@@ -0,0 +1,3 @@
+<?php
+$_['text_paysoninvoice_fee'] = 'Payson fakturaavgift';
+?>
